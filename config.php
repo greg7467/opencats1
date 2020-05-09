4c4a0cc836ae8b316c6386c4a70eb6767b983554
Greg Broomes <greg.a.broomes@gmail.com>
2020-05-09 13:59:30 -0400

    Update config.php

--- config.php
+++ config.php
@@ -495,5 +495,13 @@ class ACL_SETUP {
             'calendar.editEvent'
             'calendar.deleteEvent'
             */
+$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+
+$server = $url["host"];
+$username = $url["user"];
+$password = $url["pass"];
+$db = substr($url["path"], 1);
+
+$conn = new mysqli($server, $username, $password, $db);
 
 ?>
